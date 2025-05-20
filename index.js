d0d160eaf8a8ba900b98bad1650cb01d79c28aea
Bhoomi <bhoomigupta140520@gmail.com>
2025-05-20 22:59:10 +0530

    updated indes.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ import blogRoutes from './routes/blog.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: "https://blog-editor-frontend-beta.vercel.app", credentials: true }));
+app.use(express.json({ limit: "10mb" }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((err) => console.error(err));
